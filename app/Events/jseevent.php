05b0d2de2894d66ff768ed9145ad21f9bbf1b24d
Boris Borisov <djslinger77@gmail.com>
2019-04-19 19:18:24 -0400

    Laravel echo and pusher added

---
+++ app/Events/jseevent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+// ShouldBroadcastNow - event will not be placed to a que
+
+class jseevent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $payload; // The public variable which can be read in the event listener as e._variable_name. e.update
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('jseprod');
+    }
+}

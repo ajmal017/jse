6d3b7db25e6e9585192d37f2623a98f94685a97f
Boris Borisov <djslinger77@gmail.com>
2019-07-15 19:56:46 +0000

    jse-247

--- app/Classes/WebSocket/Front/LimitOrderMessage.php
+++ app/Classes/WebSocket/Front/LimitOrderMessage.php
@@ -365,7 +365,8 @@ class LimitOrderMessage
             //$response = \App\Classes\Trading\Exchange::amendOrder($bid - self::$limitOrderObj['step'], Cache::get('bot_' . self::$botId)['orderID'], $botSettings);
             \App\Jobs\AmendOrder::dispatch(
                 $bid,
-                Cache::get('bot_' . self::$botId)['orderID'],
+                (array_key_exists('orderID', Cache::get('bot_' . self::$botId) ?
+                    Cache::get('bot_' . self::$botId)['orderID'] : 33445566)),
                 $botSettings,
                 $amendReason
             );

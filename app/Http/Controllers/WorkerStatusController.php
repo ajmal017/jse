085648b58658a21f1d4b13ff06d0c98b55179033
Boris Borisov <djslinger77@gmail.com>
2019-08-02 17:45:54 +0000

    Worker status controller updated

--- app/Http/Controllers/WorkerStatusController.php
+++ app/Http/Controllers/WorkerStatusController.php
@@ -13,29 +13,35 @@ use Illuminate\Support\Facades\DB;
  */
 class WorkerStatusController extends Controller
 {
-    private $response;
+    private $response = array();
     public function get($botId){
         $row = DB::table('bots')
             ->where('id',$botId)
             ->get()[0];
 
         if (time() - $row->front_worker_update_time > 10){
-            $this->response = 'Front worker is offline<br>';
+            //$this->response = 'Front worker is offline<br>';
+            $this->response['isFrontWorkerRunning'] = false;
         }
         else {
-            $this->response = 'Front worker is online<br>';
+            //$this->response = 'Front worker is online<br>';
+            $this->response['isFrontWorkerRunning'] = true;
         }
 
         if (time() - $row->execution_worker_update_time > 10){
-            $this->response .= 'Execution worker is offline<br>';
+            //$this->response .= 'Execution worker is offline<br>';
+            $this->response['isExecutionWorkerRunning'] = false;
         } else {
-            $this->response .= 'Execution worker is online<br>';
+            //$this->response .= 'Execution worker is online<br>';
+            $this->response['isExecutionWorkerRunning'] = true;
         }
 
         if (time() - $row->que_worker_update_time > 10){
-            $this->response .= 'Que worker is offline<br>';
+            //$this->response .= 'Que worker is offline<br>';
+            $this->response['isQueWorkerRunning'] = false;
         } else {
-            $this->response .= 'Que worker is online<br>';
+            //$this->response .= 'Que worker is online<br>';
+            $this->response['isQueWorkerRunning'] = true;
         }
 
         return($this->response);

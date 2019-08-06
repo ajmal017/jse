6250c71b7a20bb44148f7ceafebd9537ca085d7b
Boris Borisov <djslinger77@gmail.com>
2019-08-06 23:57:43 +0000

    Fix

--- app/Classes/Trading/History.php
+++ app/Classes/Trading/History.php
@@ -54,38 +54,4 @@ class History
             ));
         }
     }
-
-    public static function loadPeriod($botSettings){
-        $barsToLoad = $botSettings['barsToLoad'];
-        $timeFrame = $botSettings['timeFrame'] . 'm';
-        $symbol = $botSettings['historySymbolName'];
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,
-            "https://www.bitmex.com/api/v1/trade/bucketed?binSize=$timeFrame&partial=false&symbol=$symbol&count=$barsToLoad&reverse=true");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        $bars = json_decode(curl_exec($ch));
-
-        if (curl_errno($ch)) {
-            throw new \Exception(curl_error($ch));
-        }
-
-        curl_close($ch);
-        if (!$bars) throw new \Exception('History is not loaded. Symbol may be wrong. Die. History.php' );
-        DB::table($botSettings['botTitle'])->truncate();
-
-        foreach(array_reverse($bars) as $bar){
-            DB::table($botSettings['botTitle'])->insert(array(
-                'symbol' => $symbol,
-                'date' => gmdate("Y-m-d G:i:s", strtotime($bar->timestamp)), // Regular date
-                'time_stamp' => strtotime($bar->timestamp) * 1000, // Timestamp
-                'open' => $bar->open,
-                'close' => $bar->close,
-                'high' => $bar->high,
-                'low' => $bar->low,
-                'volume' => $bar->volume,
-            ));
-        }
-    }
 }
